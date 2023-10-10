9f9710745c01ca6460d12633b0bebd9de45187d5
Ethereal <36712636+tvtreemachine@users.noreply.github.com>
2023-10-10 12:55:50 -0400

    Update base1.ps1

--- base1.ps1
+++ base1.ps1
@@ -1,1 +1,13 @@
-#test
+#If you want to find out which program (process) is listening on a specific port on your computer, 
+#use the following command (where 443 is a port number you want to check):
+Get-Process -Id (Get-NetTCPConnection -LocalPort 443).OwningProcess | ft Id, ProcessName, UserName, Path
+
+
+
+#list TCP open ports
+Get-NetTcpConnection -State Listen | Select-Object LocalAddress,LocalPort| Sort-Object -Property LocalPort | Format-Table
+
+
+#list UDP open ports and what service they are connected to
+Get-NetUDPEndpoint | Where {$_.LocalAddress -eq "0.0.0.0"} | select LocalAddress,LocalPort,@{Name="Process";Expression={(Get-Process -Id $_.OwningProcess).ProcessName}}
+
