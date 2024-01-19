8ca7e4ca08290e57db2edc0bd84c2a65ca9ba3bc
23younesm <64438560+23younesm@users.noreply.github.com>
2024-01-19 15:18:15 -0500

    removes date and color feature. Added output ports. Locked down everything else.

--- CLOSE.sh
+++ CLOSE.sh
@@ -1,10 +1,8 @@
-DATE=`date +%m-%d-%y@%H)` 
-DATE2=`date +%H:%M:%S)` 
-RED='\033[0;31m'
-NOCOLOR='\033[0m'
-mkdir Logs
+#DATE=`date +%m-%d-%y@%H)` 
+#DATE2=`date +%H:%M:%S)` 
+#mkdir Logs
 echo "Welcome\n"
-echo "Input open ports by ${RED}FILE${NOCOLOR} or ${RED}INPUT${NOCOLOR}\n"
+echo "Input open ports by FILE or INPUT\n"
 read file_input
 echo "\n"
 if [ "$file_input" = "FILE" ]; 
@@ -14,12 +12,14 @@ if [ "$file_input" = "FILE" ];
   	echo "$file_path Sucessfully Imported!"
   	while IFS= read -r line
 		do
-		sudo iptables -I INPUT -p tcp --dport "$line" -j ACCEPT
+		sudo iptables -I INPUT -p tcp --dport $line -j ACCEPT
+		sudo iptables -I OUTPUT -p tcp --sport $line -j ACCEPT 
 		echo $line "opened"
 		done < "$file_path"
 	
-  	sudo iptables -P INPUT DROP 
-  	echo "All other ${RED}INPUT${NOCOLOR} ports closed successfully!"
+  	sudo iptables -A INPUT -j DROP
+	sudo iptables -A OUTPUT -j DROP 
+  	echo "All other INPUT and OUTPUT ports closed successfully!\n"
   	sudo iptables -L
 	exit 1
 
@@ -28,19 +28,21 @@ else [ "$file_input" = "INPUT" ];
   read -p "Please enter ports number separated by space: " port_num
 	#echo $port_num >> "$DATE.txt"
 	echo "\n"
-	echo "#newrun-""$DATE2" >> "./Logs/$DATE.txt"
-	for port in $port_num
-		do
-		echo $port >> "./Logs/$DATE.txt"
-		done
-	echo "File with ports logged: $DATE.txt"
+#	echo "#newrun-""$DATE2" >> "./Logs/$DATE.txt"
+#	for port in $port_num
+#		do
+#		echo $port >> "./Logs/$DATE.txt"
+#		done
+#	echo "File with ports logged: $DATE.txt"
 	for port in $port_num
 		do
 			sudo iptables -I INPUT -p tcp --dport $port -j ACCEPT
+			sudo iptables -I OUTPUT -p tcp --sport $port -j ACCEPT 
 		echo $port "opened"
 		done
-	sudo iptables -P INPUT DROP 
-  	echo "All other ${RED}INPUT${NOCOLOR} ports closed successfully!"
+	sudo iptables -A INPUT -j DROP
+	sudo iptables -A OUTPUT -j DROP 
+  	echo "All other INPUT and OUTPUT ports closed successfully!\n"
   	sudo iptables -L
   	exit 1	
 fi
